cede914c7305eb61b3522c975b42e34ff8453e8c
Seth Cohen <scohen@wayfair.com>
2018-09-10 09:50:45 -0400

    [WayScan] FTP fix, additional orientation, split scan type

--- dialogs/messagedialog.py
+++ dialogs/messagedialog.py
@@ -10,11 +10,14 @@ class MessageDialog(Qtw.QDialog, msgdialog_auto.Ui_Dialog):
     have that much capability and has troubles with sizing
 
     """
-    def __init__(self, button_text, title, text, information_text, image, is_choice=False):
+    def __init__(self, button_text, title, text, information_text, image, is_choice=False, no_button_text=None):
         super().__init__()
         self.setupUi(self)
 
         self.button.setText(button_text)
+        if no_button_text is not None:
+            self.no_button.setText(no_button_text)
+            
         self.setWindowTitle(title)
         self.text.setText(text)
         self.information_text.setText(information_text)
