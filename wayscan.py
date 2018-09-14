bc68b2a9766fe2dfc91a0948540c97717fb13907
Seth Cohen <scohen@wayfair.com>
2018-09-14 15:50:04 -0400

    Added check for remaining space

--- wayscan.py
+++ wayscan.py
@@ -87,6 +87,19 @@ class MainWindow(Qtw.QMainWindow, main.Ui_MainWindow):
         
         splash.showMessage('Setting Camera Preview Image')
         self.reset_previews()
+
+        st = os.statvfs(os.path.expanduser('~'))
+        gigs_free = st.f_bavail * st.f_frsize / 1024 / 1024 / 1024
+
+        if gigs_free < 200:
+            Qtw.QMessageBox.critical(
+                self,
+                'Low Free Disk Space',
+                ('Please delete old desktop directories '
+                 'space is down to {:.2f} GB remaining.'
+                 'Open file explorer to "/home/pi/ and delete '
+                 'the dated directories only YYYYMMDD_XXXXXX'.format(gigs_free))
+            )
         
     def setup_hardware(self):
         self.cameras = [];
